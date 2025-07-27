6e3725ebf75ae7d336473eafef462557c4699afd
Chioma <okekelinda9991@gmail.com>
2025-07-27 23:31:08 +0100

    first commit

---
+++ src/app/components/Base.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Image from "next/image";
+
+// First Section
+const Base = () => {
+  return (
+    <div className="p-4 bg-slate-700 rounded-lg flex justify-between">
+      <div className="flex gap-2">
+        <Image
+          src="/logo.svg"
+          alt="Brand Logo"
+          className="w-36 lg:w-36"
+          width={200}
+          height={200}
+        />
+      </div>
+      
+      <div className="rounded-lg flex items-center p-2 bg-slate-800 cursor-pointer">
+        <Image
+          src="/icon-sun.svg"
+          alt="Light mode icon"
+          className="w-4 lg:w-6"
+          width={200}
+          height={200}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Base;
